9c2bdb1961e452304630d3b53d9aa3071b4f6cb4
Mauricio Minella <mauriciominella@gmail.com>
2014-06-17 18:31:20 -0300

    RoundRunActivity changed to call some new methods from the Run controller that are not created. The code is broken.

--- CapigoaTrainer/app/src/main/java/com/bravi/capigoatrainer/app/RoundRunActivity.java
+++ CapigoaTrainer/app/src/main/java/com/bravi/capigoatrainer/app/RoundRunActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.bravi.capigoatrainer.domain.RoundRunController;
@@ -11,21 +14,50 @@ import com.bravi.capigoatrainer.domain.RoundRunController;
 
 public class RoundRunActivity extends ActionBarActivity {
 
+    RoundRunController roundController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_round_run);
 
         Bundle b = getIntent().getExtras();
         int roundNumber = b.getInt(MainActivity.ROUND_NUMBER_KEY);
 
-        RoundRunController roundController = new RoundRunController(roundNumber);
-        roundController.Start();
+        roundController = new RoundRunController();
+        roundController.Start(roundNumber);
 
         TextView roundTitle = (TextView)findViewById(R.id.txtRoundRunTitle);
         roundTitle.setText("Round " + Integer.toString(roundNumber));
+
+        TextView wordNumber = (TextView)findViewById(R.id.txt_wordNumber);
+        wordNumber.setText(String.format("{0}/{1}"), roundController.getCurrentVerbNumber(), roundController.getVerbCount());
+
+
+        TextView score = (TextView)findViewById(R.id.txt_score);
+        score.setText(String.format("{0}%", roundController.getPercentScore()));
+
+        TextView infinitiveVerb = (TextView)findViewById(R.id.txt_infinitiveVerb);
+        infinitiveVerb.setText(roundController.getCurrentVerb().infinitive);
+
+        TextView currentVerbTense = (TextView)findViewById(R.id.txt_currentVerbTense);
+        currentVerbTense.setText(roundController.getCurrentRequiredTense());
+
+        EditText verbInput = (EditText)findViewById(R.id.txt_verbInput);
+
+        Button nextVerbButton = (Button)findViewById(R.id.btn_NextVerb);
+        nextVerbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HandleNextButtonClick();
+            }
+        });
     }
 
+    private void HandleNextButtonClick() {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
